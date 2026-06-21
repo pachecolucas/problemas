27f0828ae6bcc4912572db5372fd1a5b998e62a6
Lucas Pacheco Teixeira <lucas@eliti.com.br>
2026-06-21 10:46:08 -0300

    Update index.tsx

--- app/home/index.tsx
+++ app/home/index.tsx
@@ -6,7 +6,7 @@ type Props = {
 };
 export default function Index({ grupos }: Props) {
   return (
-    <div className="flex min-h-screen">
+    <div className="flex flex-col lg:flex-row min-h-screen">
       {grupos.map((g) => (
         <ComponenteGrupo key={g.letra} grupo={g} />
       ))}
@@ -19,7 +19,7 @@ function ComponenteGrupo({ grupo }: { grupo: Grupo }) {
     <div className="flex-1 flex flex-col gap-4 p-4 py-4" style={{ background: grupo.cores.forte }}>
       <div className="text-center" style={{ color: grupo.cores.claro }}>
         <div className="text-6xl">{grupo.icone}</div>
-        <h1 className="uppercase font-black">{grupo.nome}</h1>
+        <h1 className="uppercase font-black text-xl">{grupo.nome}</h1>
         <p>{grupo.descricao}</p>
       </div>
       {grupo.transtornos.map((t) => (
@@ -46,21 +46,23 @@ function ComponenteTranstorno({ t, g }: { t: Transtorno; g: Grupo }) {
           </div>
         </AccordionTrigger>
         <AccordionContent className="flex flex-col gap-4">
-          <h3 className="font-semibold flex gap-1 mb-2">
-            <span>Critérios</span>
-            <span className="font-normal text-black/70">
-              ({t.minimoCriterios} de {t.criterios.length})
-            </span>
-            <span>:</span>
-          </h3>
-          <ul className="ml-4 p-0 flex flex-col gap-2">
-            {t.criterios.map((c, ci) => (
-              <li key={ci} className="flex gap-2">
-                <span>●</span>
-                <span>{c}</span>
-              </li>
-            ))}
-          </ul>
+          <div>
+            <h3 className="font-semibold flex gap-1 mb-2">
+              <span>Critérios</span>
+              <span className="font-normal text-black/70">
+                ({t.minimoCriterios} de {t.criterios.length})
+              </span>
+              <span>:</span>
+            </h3>
+            <ul className="ml-4 p-0 flex flex-col gap-2">
+              {t.criterios.map((c, ci) => (
+                <li key={ci} className="flex gap-2">
+                  <span>●</span>
+                  <span>{c}</span>
+                </li>
+              ))}
+            </ul>
+          </div>
           <div className="text-balance flex gap-2 bg-white rounded shadow p-4">
             <div>{t.signo.icone}</div>
             <div>{t.signo.descricao}</div>
